2f6ef7bdbd970e77654ef8f811d7f33ec5f22cc7
Juan Carlos Pinzon <juancarlospinzon@outlook.com>
2018-11-04 21:26:36 -0500

    Final

---
+++ clients/commands.py
@@ -0,0 +1,95 @@
+import click
+
+from clients.services import ClientService
+from clients.models import Client
+
+@click.group()
+def clients():
+  """Manages the clients lifecycle"""
+  pass
+
+
+@clients.command()
+@click.option('-n', '--name',
+							type=str,
+							prompt=True,
+							help='The client name')
+@click.option('-c', '--company',
+							type=str,
+							prompt=True,
+							help='The client company')
+@click.option('-e', '--email',
+							type=str,
+							prompt=True,
+							help='The client email')
+@click.option('-p', '--position',
+							type=str,
+							prompt=True,
+							help='The client position')						
+@click.pass_context
+def create(ctx, name, company, email, position):
+	"""Create a new Client"""
+	client = Client(name, company, email, position)
+
+	client_service = ClientService(ctx.obj['clients_table'])
+	client_service.create_client(client)
+
+
+@clients.command()
+@click.pass_context
+def list_clients(ctx):
+	"""List all clients"""
+	client_service = ClientService(ctx.obj['clients_table'])
+
+	clients_list = client_service.list_clients()
+
+	click.echo('ID  |  NAME  |  COMPANY  |  EMAIL  |  POSITION')
+	print('*' * 50)
+
+	for client in clients_list:
+		click.echo('{}  |  {}  |  {}  |  {}'.format(
+			client['uid'], client['name'], client['company'], client['position']
+		))
+
+
+@clients.command()
+@click.pass_context
+def update(ctx, client_uid):
+	"""Update a client"""
+	client_service = ClientService(ctx.obj['clients_table'])
+
+	clients_list = client_service.list_clients()
+	client = [client for client in clients_list if client['uid'] == client_uid]
+
+	if client:
+		client = _update_client_flow(Client(**client[0]))
+		client_service.update_client(client)
+
+		click.echo('Client updated')
+	else:
+		click.echo('Client not found')
+
+	def _update_client_flow(client):
+		click.echo('Leave empty if you don\'t want to modify a value')
+
+		client.name = click.prompt('New name', type=str, default=client.name)
+		client.company = click.prompt('New company', type=str, default=client.company)
+		client.email = click.prompt('New email', type=str, default=client.email)
+		client.position = click.prompt('New position', type=str, default=client.position)
+
+		return client
+
+
+@clients.command()
+@click.pass_context
+def delete(ctx, client_uid):
+	"""Delete a client"""
+	client_uid = click.prompt('Client UID to delet', type=str)
+	lient_service = ClientService(ctx.obj['clients_table'])
+	clients_list = client_service.list_clients()
+	client = [client for client in clients_list if client['uid'] != client_uid]
+
+	client_service._save_to_disk(client)
+
+
+all = clients
